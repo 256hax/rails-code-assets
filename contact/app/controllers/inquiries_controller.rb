a13721f50bc7b6906339c774d867ba93c0395230
256hax <256hax@github>
2018-12-09 19:33:09 +0900

    chore: add dotenv-rails

--- contact/app/controllers/inquiries_controller.rb
+++ contact/app/controllers/inquiries_controller.rb
@@ -14,8 +14,16 @@ class InquiriesController < ApplicationController
   def create
     @inquiry = Inquiry.new(inquiry_params)
     if @inquiry.save
-      api = Kintone::Api.new("{サブドメイン名}.cybozu.com", "{APIトークン}")
-      app = {アプリケーションID}
+      # ENV => require "/.env" file
+      kintone_service_url = "#{ENV["KINTONE_SUBDOMAIN"]}.cybozu.com"
+      kintone_api_token = ENV["KINTONE_API_TOKEN"]
+      # api = Kintone::Api.new("{サブドメイン名}.cybozu.com", "{APIトークン}")
+      api = Kintone::Api.new(kintone_service_url, kintone_api_token)
+      # app_id = {アプリケーションID}
+      app_id = ENV["KINTONE_CONTACT_APP_ID"]
+      p kintone_service_url
+      p kintone_api_token
+      p app_id
 
       # Record register(single record)
       # Use Hash
@@ -24,7 +32,7 @@ class InquiriesController < ApplicationController
                 "QType" => {"value" => inquiry_params[:contact_type]},
                 "Detail" => {"value" => inquiry_params[:details]}
                 }
-      api.record.register(app, record)
+      api.record.register(app_id, record)
       redirect_to root_path
     else
       render 'new'
